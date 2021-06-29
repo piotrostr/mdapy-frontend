3ec47cdd1625552dd0bbb6b4190ed5c7eb380dbb
piotr-ost <piotr.ostrowski@aol.com>
2021-06-29 19:23:23 +0200

    add placeholder data

--- src/pages/index.js
+++ src/pages/index.js
@@ -34,9 +34,18 @@ export default function Main() {
     table: {
       columnLabels: [],
       data: [
-        ['foo', 'bar', 'baz'], 
-        [], [], [], [], [], [], [], [], []
-      ]
+        [{'value': 'UK027'}, {'value': '1167,3'}, {'value': '4,9'}], 
+        [{'value': 'UK026'}, {'value': '1185,5'}, {'value': '4,8'}],
+        [{'value': 'UK026'}, {'value': '1201,6'}, {'value': '4,8'}], 
+        [{'value': 'UK027'}, {'value': '1207,5'}, {'value': '5,6'}], 
+        [{'value': 'UK026'}, {'value': '1209,6'}, {'value': '4,7'}],
+        [{'value': 'UK027'}, {'value': '1231,1'} , {'value': '15,4'}],
+        [{'value': 'UK026'}, {'value': '1258,1'}, {'value': '4,7'}],
+        [{'value': 'UK027'}, {'value': '1287,2'}, {'value': '4,7'}] , 
+        [{'value': 'UK026'}, {'value': '1326,9'}, {'value': '12,6'}],
+        [{'value': 'UK026'}, {'value': '1338,2'}, {'value': '8,9'}], 
+        [{'value': 'UK026'}, {'value': '1326,9'}, {'value': '12,6'}], 
+        [{'value': 'UK026'}, {'value': '1338,2'}, {'value': '8,9'}]]      
     },
     dataLoaded: false,
     graphReady: false,
