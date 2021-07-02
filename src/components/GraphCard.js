b3916d4dce57f8f9e7ad1a59a296dbfe7019cd3b
piotr-ost <piotr.ostrowski@aol.com>
2021-07-02 12:15:58 +0200

    amend svg plot embed styling

--- src/components/GraphCard.js
+++ src/components/GraphCard.js
@@ -4,8 +4,15 @@ import styled from 'styled-components'
 
 const Container = styled.div`
   object-fit: scale-down;
+  overflow-y: scroll;
+  display: flex;
+  justify-content: center;
   width: 100%;
   height: 100%;
+  svg {
+    width: 600pt;
+    height: 800pt;
+  }
 `
 
 export default function GraphCardPlaceholder({ svg }) {
